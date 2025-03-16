2d9e9790d259ac3cb01cd7d47a7b6cb71ce0a0f3
Sourabh  Gupta <sourabhgupta@Sourabh-MacBook-Pro.local>
2025-03-16 10:52:39 +0530

    initial

---
+++ Xperi_Image_Processing/src/main/java/com/acheron/logo/XperiImageProcessingApplication.java
@@ -0,0 +1,13 @@
+package com.acheron.logo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XperiImageProcessingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XperiImageProcessingApplication.class, args);
+	}
+
+}
---
+++ Xperi_Image_Processing/src/test/java/com/acheron/logo/XperiImageProcessingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.acheron.logo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XperiImageProcessingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
